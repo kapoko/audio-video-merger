6048bdf61ddc293fd25b78ff2958f07d4e8629bd
kapoko <17455539+kapoko@users.noreply.github.com>
2024-07-30 13:12:53 +0200

    Updated electron and ffmpeg binaries

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,37 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import * as ffmpeg from 'fluent-ffmpeg';
-import * as mime from 'mime-types';
-import { app, BrowserWindow, ipcMain, dialog , IpcMainEvent } from 'electron';
-import * as remoteMain from '@electron/remote/main';
-import { ProcessFilesRequest, SingleProcessOptions, ProcessResult, FileInfo } from './lib/interfaces';
-import { getSeconds } from './lib/helpers'
-import debounce from 'lodash/debounce';
+import * as path from "path";
+import * as fs from "fs";
+import * as ffmpeg from "fluent-ffmpeg";
+import * as mime from "mime-types";
+import { app, ipcMain, dialog, BrowserWindow } from "electron";
+import type { IpcMainEvent } from "electron";
+import * as remoteMain from "@electron/remote/main";
+import type {
+    ProcessFilesRequest,
+    SingleProcessOptions,
+    ProcessResult,
+    FileInfo,
+} from "./lib/interfaces";
+import { getSeconds } from "./lib/helpers";
+import debounce from "lodash/debounce";
 
 //Get the paths to the packaged versions of the binaries we want to use
-import ffmpegPath from 'ffmpeg-static'
-import ffprobePath from 'ffprobe-static'
+import ffmpegPath from "ffmpeg-static";
+import ffprobePath from "ffprobe-static";
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+    // eslint-disable-line global-require
     app.quit();
 }
 
-let mainWindow: BrowserWindow
-let mainWindowReady = false
-let openFiles: FileInfo[] = []
-const gotTheLock = app.requestSingleInstanceLock()
-const enableDevTools = !app.isPackaged
+let mainWindow: BrowserWindow;
+let mainWindowReady = false;
+let openFiles: FileInfo[] = [];
+const gotTheLock = app.requestSingleInstanceLock();
+const enableDevTools = !app.isPackaged;
 
 remoteMain.initialize();
 
@@ -39,30 +46,30 @@ const createWindow = (): void => {
             nodeIntegration: false,
             contextIsolation: true,
             preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
-            devTools: enableDevTools
-        }
+            devTools: enableDevTools,
+        },
     });
 
     remoteMain.enable(mainWindow.webContents);
-    
+
     mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
-    
+
     if (enableDevTools) {
         mainWindow.webContents.openDevTools();
     }
-    
-    mainWindow.once('ready-to-show', () => {
+
+    mainWindow.once("ready-to-show", () => {
         mainWindowReady = true;
-        mainWindow.show()
+        mainWindow.show();
 
         if (openFiles.length) {
             handleOpenFiles();
         }
-    })
+    });
 };
 
 function getFileInfo(path: string): FileInfo {
-    const { size } = fs.statSync(path); 
+    const { size } = fs.statSync(path);
     const type = String(mime.lookup(path));
     const file: FileInfo = { size, path, type };
 
@@ -75,7 +82,7 @@ function getFileInfo(path: string): FileInfo {
  */
 const debouncedOpenFiles: () => void = debounce(handleOpenFiles, 1000);
 
-app.on('open-file', (event, path: string) => {
+app.on("open-file", (event, path: string) => {
     event.preventDefault();
 
     // Get some file info
@@ -84,20 +91,20 @@ app.on('open-file', (event, path: string) => {
 
     if (mainWindowReady) {
         if (openFiles.length === 1) {
-            mainWindow.webContents.send('merge:waiting');
+            mainWindow.webContents.send("merge:waiting");
         }
         debouncedOpenFiles();
     }
 });
 
 if (!gotTheLock) {
-    app.quit()
+    app.quit();
 } else {
-    app.on('second-instance', () => {
+    app.on("second-instance", () => {
         // Someone tried to run a second instance, we should focus our window.
         if (mainWindow) {
-            if (mainWindow.isMinimized()) mainWindow.restore()
-            mainWindow.focus()
+            if (mainWindow.isMinimized()) mainWindow.restore();
+            mainWindow.focus();
 
             // openFiles = argv;
             // openFiles.unshift('second-instance');
@@ -105,19 +112,19 @@ if (!gotTheLock) {
             //     handleOpenFiles()
             // }
         }
-    })
-    
+    });
+
     // Create myWindow, load the rest of the app, etc...
-    app.on('ready', createWindow);
+    app.on("ready", createWindow);
 }
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
@@ -128,139 +135,179 @@ function handleOpenFiles() {
         return;
     }
 
-    mainWindow.webContents.send('merge:start', openFiles);
+    mainWindow.webContents.send("merge:start", openFiles);
     openFiles = [];
 }
-  
-ffmpeg.setFfmpegPath(ffmpegPath.replace('app.asar', 'app.asar.unpacked'));
-ffmpeg.setFfprobePath(ffprobePath.path.replace('app.asar', 'app.asar.unpacked'));
-console.log(ffprobePath.path.replace('app.asar', 'app.asar.unpacked'));
 
-function processVideo(options: SingleProcessOptions, totalBytesProcessed: number, totalBytes: number, event: IpcMainEvent) {
+if (ffmpegPath) {
+    ffmpeg.setFfmpegPath(ffmpegPath.replace("app.asar", "app.asar.unpacked"));
+}
+ffmpeg.setFfprobePath(
+    ffprobePath.path.replace("app.asar", "app.asar.unpacked"),
+);
+
+function processVideo(
+    options: SingleProcessOptions,
+    totalBytesProcessed: number,
+    totalBytes: number,
+    event: IpcMainEvent,
+) {
     return new Promise((resolve, reject) => {
         let duration: number | false;
-        
-        ffmpeg.default()
+
+        ffmpeg
+            .default()
             .input(options.video.path)
             .addInput(options.audio.path)
-            .outputOptions([
-                '-c:v copy',
-                '-map 0:v:0',
-                '-map 1:a:0'
-            ])
-            .on('codecData', data => duration = data.video_details ? getSeconds(data.duration) : false)
-            .on('end', resolve)
-            .on('error', error => reject(error.message))
-            .on('progress', (progress) => {
-                const timemark = getSeconds(progress.timemark)
-                if (duration && timemark) { // Only send progress if we have enough info
+            .outputOptions(["-c:v copy", "-map 0:v:0", "-map 1:a:0"])
+            .on(
+                "codecData",
+                (data) =>
+                    (duration = data.video_details ? getSeconds(data.duration) : false),
+            )
+            .on("end", resolve)
+            .on("error", (error) => reject(error.message))
+            .on("progress", (progress) => {
+                const timemark = getSeconds(progress.timemark);
+                if (duration && timemark) {
+                    // Only send progress if we have enough info
                     const currentProgress = Math.min(timemark / duration, 1);
                     const currentBytesProcessed = currentProgress * options.bytes;
-                    event.reply('merge:progress', (totalBytesProcessed + currentBytesProcessed) / totalBytes)
+                    event.reply(
+                        "merge:progress",
+                        (totalBytesProcessed + currentBytesProcessed) / totalBytes,
+                    );
                 }
             })
             .save(options.output);
-    }); 
+    });
 }
 
-ipcMain.on('merge', async (event, input: ProcessFilesRequest) => {
+ipcMain.on("merge", async (event, input: ProcessFilesRequest) => {
     const { videoList, audioList } = input;
     let checksFailed = false;
-    
+
     const processChain: SingleProcessOptions[] = [];
     let yesToAll = false;
     let noToAll = false;
 
     // Pre-render checks
-    audioList.forEach(audio => {
+    audioList.forEach((audio) => {
         const filename = path.basename(audio.path, path.extname(audio.path));
-        if (videoList.map(video => path.basename(video.path, path.extname(video.path))).includes(filename)) {
+        if (
+            videoList
+                .map((video) => path.basename(video.path, path.extname(video.path)))
+                .includes(filename)
+        ) {
             dialog.showMessageBoxSync(mainWindow, {
-                type: 'error',
-                message: `Trying to convert ${path.basename(audio.path)} but a video with the same name is being used as a source file. Please rename the source video.`
+                type: "error",
+                message: `Trying to convert ${path.basename(audio.path)} but a video with the same name is being used as a source file. Please rename the source video.`,
             });
-            event.reply('merge:cancel');
+            event.reply("merge:cancel");
             checksFailed = true;
         }
-    })
+    });
 
     if (checksFailed) {
         return;
     }
-    
+
     // Loop over all videos and audio
-    videoList.forEach(video => {
-        audioList.forEach(audio => {
+    videoList.forEach((video) => {
+        audioList.forEach((audio) => {
             const dir = path.dirname(audio.path);
-            const fileName = path.basename(audio.path, path.extname(audio.path)) + path.extname(video.path);
+            const fileName =
+                path.basename(audio.path, path.extname(audio.path)) +
+                path.extname(video.path);
             const videoBaseName = path.basename(video.path, path.extname(video.path));
-            
-            const output = videoList.length > 1 ? path.join(dir, videoBaseName + '_' + fileName) : path.join(dir, fileName);            
+
+            const output =
+                videoList.length > 1
+                    ? path.join(dir, videoBaseName + "_" + fileName)
+                    : path.join(dir, fileName);
 
             // Check if file already exists
             if (fs.existsSync(output) && !yesToAll && !noToAll) {
-
                 // Check if trying to write over a file that's used as a source file
                 if (path.basename(video.path) === path.basename(output)) {
-                    console.log('yeaa');
+                    console.log("yeaa");
                 }
 
                 const result: number = dialog.showMessageBoxSync(mainWindow, {
-                    type: 'question',
-                    message: 'The file ' + path.basename(output) + ' already exists in the source folder. Overwrite it?',
-                    buttons: ['Yes to all', 'Yes', 'Cancel']
-                })
-                
+                    type: "question",
+                    message:
+                        "The file " +
+                        path.basename(output) +
+                        " already exists in the source folder. Overwrite it?",
+                    buttons: ["Yes to all", "Yes", "Cancel"],
+                });
+
                 switch (result) {
                 case 0: // Yes to all
                     yesToAll = true;
                     break;
                 case 1: // Yes (do nothing)
                     break;
-                case 2: // Cancel 
-                    event.reply('merge:cancel')
+                case 2: // Cancel
+                    event.reply("merge:cancel");
                     noToAll = true;
                     break;
                 }
             }
-            
-            processChain.push({ video, audio, output, bytes: audio.size + video.size});
+
+            processChain.push({
+                video,
+                audio,
+                output,
+                bytes: audio.size + video.size,
+            });
         });
     });
-    
+
     if (noToAll) {
         return;
     }
-    
-    const result: ProcessResult = { 
-        processed: 0, 
+
+    const result: ProcessResult = {
+        processed: 0,
         total: input.numVideos,
-        errors: []
-    }
-    
+        errors: [],
+    };
+
     // Gather total bytesize to process for making an estimation on the progress
-    const totalBytes: number = processChain.reduce((total, process) => total += process.bytes, 0)
+    const totalBytes: number = processChain.reduce(
+        (total, process) => (total += process.bytes),
+        0,
+    );
     let bytesProcessed = 0;
-    
+
     for (const current of processChain) {
-        await processVideo(current, bytesProcessed, totalBytes, event).catch(result.errors.push);
+        await processVideo(current, bytesProcessed, totalBytes, event).catch(
+            result.errors.push,
+        );
         bytesProcessed += current.bytes;
         result.processed++;
-        event.reply('merge:progress', bytesProcessed / totalBytes)
+        event.reply("merge:progress", bytesProcessed / totalBytes);
     }
-    
-    event.reply(result.errors.length ? 'merge:error' : 'merge:complete', result);
+
+    event.reply(result.errors.length ? "merge:error" : "merge:complete", result);
 });
 
-ipcMain.on('showDialog', (event, options) => {
+ipcMain.on("showDialog", (event, options) => {
     dialog.showMessageBox(mainWindow, options);
 });
 
-ipcMain.on('showOpenDialog', (event, options) => {
-    const filePaths: string[] | undefined = dialog.showOpenDialogSync(mainWindow, options);
+ipcMain.on("showOpenDialog", (event, options) => {
+    const filePaths: string[] | undefined = dialog.showOpenDialogSync(
+        mainWindow,
+        options,
+    );
     if (filePaths) {
-        event.reply('showOpenDialog:response', filePaths.map(path => getFileInfo(path)));
+        event.reply(
+            "showOpenDialog:response",
+            filePaths.map((path) => getFileInfo(path)),
+        );
     } else {
-        event.reply('showOpenDialog:response', []);
+        event.reply("showOpenDialog:response", []);
     }
 });
