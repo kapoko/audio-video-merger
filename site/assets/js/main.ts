b2c52386f88a7e927ac03a12ee1d2c140cb3e440
kapoko <17455539+kapoko@users.noreply.github.com>
2026-04-20 15:46:06 +0200

    Simpler setup for the site

--- site/assets/js/main.ts
+++ site/assets/js/main.ts
@@ -4,8 +4,6 @@ import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faGithub, faWindows, faApple, faPaypal} from "@fortawesome/free-brands-svg-icons";
 import {} from "@fortawesome/free-solid-svg-icons";
 
-import '../sass/main.scss';
-
 library.add(faWindows, faApple, faGithub, faPaypal);
 dom.watch();
 
