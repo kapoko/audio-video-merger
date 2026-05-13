a1776c4fffa7d842d08d2e67338a21145962259f
kapoko <17455539+kapoko@users.noreply.github.com>
2026-05-13 11:50:02 +0200

    Add license and link to license on website

--- site/assets/js/main.ts
+++ site/assets/js/main.ts
@@ -2,9 +2,9 @@ import domReady from './helpers/domReady';
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faGithub, faWindows, faApple, faPaypal} from "@fortawesome/free-brands-svg-icons";
-import {} from "@fortawesome/free-solid-svg-icons";
+import { faScaleBalanced } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faWindows, faApple, faGithub, faPaypal);
+library.add(faWindows, faApple, faGithub, faPaypal, faScaleBalanced);
 dom.watch();
 
 domReady(() => {
