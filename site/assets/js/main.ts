ea7315c06c35360350aaf04074e6ac8a23e543bd
kapoko <17455539+kapoko@users.noreply.github.com>
2026-05-20 22:26:26 +0200

    Add more apps link

--- site/assets/js/main.ts
+++ site/assets/js/main.ts
@@ -2,9 +2,9 @@ import domReady from './helpers/domReady';
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faGithub, faWindows, faApple, faPaypal} from "@fortawesome/free-brands-svg-icons";
-import { faScaleBalanced } from "@fortawesome/free-solid-svg-icons";
+import { faScaleBalanced, faLayerGroup } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faWindows, faApple, faGithub, faPaypal, faScaleBalanced);
+library.add(faWindows, faApple, faGithub, faPaypal, faScaleBalanced, faLayerGroup);
 dom.watch();
 
 domReady(() => {
